7a0bb2f52f38af361ce2b3ccb980b6c2dc5a28df
Abdelrahman <slim2001.msn@gmail.com>
2022-10-22 19:33:14 +0200

    fix noise and reconstruction

--- helper.py
+++ helper.py
@@ -1,35 +1,30 @@
 import numpy as np
 import random
+
+
 def sampling_func(Sampling_Freq, time, signals):
-    x2 = 0
-    T = 1 / Sampling_Freq
-    n = np.arange(0, time / T)
-    nT = n * T
+    sampled_func = 0
+    n = np.linspace(0, time, int(time*Sampling_Freq))
     for signal in signals:
-        x2+=signal["Amplitude"]*np.sin(2 * np.pi * signal["Frequency"] * nT)
-    return nT, x2
+        sampled_func += signal["Amplitude"] * \
+            np.sin(2 * np.pi * signal["Frequency"] * n)
+    return n, sampled_func
 
 
 def add_noise(SNR):
     noise_amp = 0
     if SNR >= 0:
-        noise_amp = 0.0008/(SNR+0.01)
+        noise_amp = 0.00008/(SNR+1)
     else:
         noise_amp = -1 * SNR * 0.0017
 
-    noise = noise_amp * np.asarray(random.sample(range(0, 1000), 500))
+    noise = noise_amp * np.asarray(random.sample(range(0, 500), 500))
 
     return noise
 
 
 def reconstruction(t, frquency_rate, sample_points, y_sampled):
-    time_rate = 1/frquency_rate
-    y = 0
+    reconstructed_func = 0
     for i in range(0, sample_points, 1):
-        y += y_sampled[i] * \
-            np.sin(np.pi*frquency_rate*(t - i*time_rate)) / \
-            ((np.pi*frquency_rate*(t - i*time_rate)))
-    return y
-
-
-
+        reconstructed_func += y_sampled[i] * np.sinc(t*frquency_rate-i)
+    return reconstructed_func
